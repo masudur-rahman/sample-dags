9f09a079ea165dcd21a86fce88633c94f69cf413
Masudur Rahman <masudjuly02@gmail.com>
2024-05-16 15:42:47 +0600

    Write to database (connection from airflow connection)
    Signed-off-by: Masudur Rahman <masudjuly02@gmail.com>

---
+++ dags/db-write.py
@@ -0,0 +1,36 @@
+import datetime
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+# Define the default arguments
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+    'retry_delay': datetime.timedelta(minutes=5),
+}
+
+# Define the DAG
+with DAG(
+    dag_id="postgres_operator_dag",
+    default_args=default_args,
+    start_date=datetime.datetime(2020, 2, 2),
+    schedule_interval="@once",
+    catchup=False,
+) as dag:
+
+    # Task to create the pet table
+    create_pet_table = PostgresOperator(
+        task_id="create_pet_table",
+        postgres_conn_id="pg-conn",
+        sql="sql/pet_schema.sql",
+    )
+
+    # Task to populate the pet table
+    populate_pet_table = PostgresOperator(
+        task_id="populate_pet_table",
+        postgres_conn_id="pg-conn",
+        sql="sql/pet_insert.sql",
+    )
+
+    # Set task dependencies
+    create_pet_table >> populate_pet_table
